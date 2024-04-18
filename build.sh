0c94d8bcb9f0e24bce037cc4d6e28159fcf878ec
pkgdemon <pkgdemonteam@gmail.com>
2024-04-18 01:17:49 -0400

    Set src based on arch

--- build.sh
+++ build.sh
@@ -8,11 +8,29 @@ CPUS=$(nproc)
 export GNUSTEP_INSTALLATION_DOMAIN=SYSTEM
 . /Developer/Makefiles/GNUstep.sh
 
-ls libobjc2
-mkdir libobjc2/Build
-pwd
-ls
-cd libobjc2/Build && pwd && ls && cmake .. \
+if [ -f "/__w/system/system/root_amd64.zip" ]; then
+  export SRC="/__w/system/system/"
+fi
+
+if [ -f "/home/runner/work/system/system/root_arm64.zip" ]; then
+  export SRC="/home/runner/work/system/system/"  
+fi
+
+mkdir ${SRC}/swift-corelibs-libdispatch/Build
+cd ${SRC}/swift-corelibs-libdispatch/Build && cmake .. \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_INSTALL_PREFIX=/System \
+  -DCMAKE_INSTALL_LIBDIR=/System/lib \
+  -DCMAKE_INSTALL_MANDIR=/System/Documentation/man \
+  -DINSTALL_PRIVATE_HEADERS=YES
+gmake -j"${CPUS}" || exit 1
+gmake install
+rm /System/include/Block_private.h
+
+mkdir ${SRC}/ libobjc2/Build
+cd ${SRC}/libobjc2/Build && pwd && ls && cmake .. \
   -DGNUSTEP_INSTALL_TYPE=SYSTEM \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_C_COMPILER=clang \
@@ -20,11 +38,11 @@ cd libobjc2/Build && pwd && ls && cmake .. \
 gmake -j"${CPUS}" || exit 1
 gmake install
 
-cd libs-base && ./configure --with-installation-domain=SYSTEM && gmake -j"${CPUS}" || exit 1 && gmake install
-cd libs-gui && ./configure && gmake -j"${CPUS}" || exit 1 || exit 1 && gmake install
-cd libs-back && ./configure && gmake -j"${CPUS}" || exit 1 && gmake install
-cd apps-gworkspace && ./configure && gmake && gmake install
-cd apps-systempreferences && gmake -j"${CPUS}" && gmake install
-cd dubstep-dark-theme && gmake -j"${CPUS}" && gmake install
-cd dubstep-dark-theme && gmake -j"${CPUS}" && gmake install
-cd gs-terminal && gmake -j"${CPUS}" && gmake install
+cd ${SRC}/libs-base && ./configure --with-installation-domain=SYSTEM && gmake -j"${CPUS}" || exit 1 && gmake install
+cd ${SRC}/libs-gui && ./configure && gmake -j"${CPUS}" || exit 1 || exit 1 && gmake install
+cd ${SRC}/libs-back && ./configure && gmake -j"${CPUS}" || exit 1 && gmake install
+cd ${SRC}/apps-gworkspace && ./configure && gmake && gmake install
+cd ${SRC}/apps-systempreferences && gmake -j"${CPUS}" && gmake install
+cd ${SRC}/dubstep-dark-theme && gmake -j"${CPUS}" && gmake install
+cd ${SRC}/dubstep-dark-theme && gmake -j"${CPUS}" && gmake install
+cd ${SRC}/gs-terminal && gmake -j"${CPUS}" && gmake install
