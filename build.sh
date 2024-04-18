6631f28d7d43d0f580274609b69850e04cc2f5e7
pkgdemon <pkgdemonteam@gmail.com>
2024-04-18 00:45:38 -0400

    Remove libdispatch for now

--- build.sh
+++ build.sh
@@ -8,19 +8,6 @@ CPUS=$(nproc)
 export GNUSTEP_INSTALLATION_DOMAIN=SYSTEM
 . /Developer/Makefiles/GNUstep.sh
 
-mkdir swift-corelibs-libdispatch/Build
-cd swift-corelibs-libdispatch/Build && cmake .. \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_COMPILER=clang \
-  -DCMAKE_CXX_COMPILER=clang++ \
-  -DCMAKE_INSTALL_PREFIX=/System \
-  -DCMAKE_INSTALL_LIBDIR=/System/lib \
-  -DCMAKE_INSTALL_MANDIR=/System/Documentation/man \
-  -DINSTALL_PRIVATE_HEADERS=YES
-gmake -j"${CPUS}" || exit 1
-gmake install
-rm /System/include/Block_private.h
-
 mkdir libobjc2/Build
 cd libobjc2/Build && cmake .. \
   -DGNUSTEP_INSTALL_TYPE=SYSTEM \
